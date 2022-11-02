bffe1c5ed2cdeb5fc4bac7ca13406f2bb551a9ce
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2022-11-02 19:59:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='SkyScratch',
       version='1.2.0',
       description='VPN library that will duisguise you while browsing the web',
-      author='kritanta',
+      author='Rayan25062011',
       url='https://github.com/Rayan25062011/SkyScratch',
       install_requires=['requests', 'socket', 'subprocess', 'signal', 'sys', 'os'],
       packages=['SkyScratch'],
