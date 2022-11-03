f2c0f5482ef4f1ebf5b59598cbec5c9d0ebbf39d
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2022-11-03 11:41:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup
 
-setup(name='SkyScratch',
-      version='1.2.0',
-      description='VPN library that will duisguise you while browsing the web',
-      author='Rayan25062011',
-      url='https://github.com/Rayan25062011/SkyScratch',
-      install_requires=['requests', 'socket', 'subprocess', 'signal', 'sys', 'os'],
-      packages=['SkyScratch'],
-      package_dir={
-          'SkyScratch': 'SkyScratch/skyscratch'
-      },
-      packages=find_packages(where="SkyScratch"),
-      scripts=['SkyScratch/skyscratch']
-      )
+with open("README.md","r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="SkyScratch",
+    version="1.2.0",
+    description="This is VPN that will disguise 🥸 your identity and encrypt 🔒your network traffic 🚗 Also portable! 📱",
+    py_modules=["SkyScratch"],
+    package_dir={"":"SkyScratch"},
+    classifiers=["Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.1",
+                 "Programming Language :: Python :: 3.2",
+                 "Programming Language :: Python :: 3.3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 "Programming Language :: Python :: 3.7",
+                 "License :: OSI Approved :: Apache-2.0 license",
+                 "Operating System :: OS Independent"
+                ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rayan25062011/SkyScratch",
+    author="Rayan25062011"
+)
+
